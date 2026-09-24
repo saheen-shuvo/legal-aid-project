5989807389e12b9bdfd3fc0bc58da88ea0b8b113
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-09-24 14:25:08 +0600

    Project Client & Server Set Up

---
+++ legal-aid-server/src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Legal Aid Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
